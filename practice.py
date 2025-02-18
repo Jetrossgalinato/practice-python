3721efb2b9e9ecff28f4a078494f2a3f972b7180
Jetross <jetrossneri07@gmail.com>
2025-02-18 19:51:27 +0800

    learned other ways of calling variables

--- practice.py
+++ practice.py
@@ -5,3 +5,7 @@ word = "The sum of "
 sum = num1 + num2
 
 print(word + str(num1) + " and " +str(num2) + " is " + str(sum))
+
+#another way of calling the variables
+print(f"{word}{num1} and {num2} is {sum}") #this is the new way of calling variables
+print("%s %d and %d is %d" % (word, num1, num2, sum)) #this is the old way of calling variables
