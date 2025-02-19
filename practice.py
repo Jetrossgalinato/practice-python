a4c2243b7acb71edbc6bdecca7aa064f97fb6cf1
Jetross <jetrossneri07@gmail.com>
2025-02-20 06:51:17 +0800

    learned lists and power relationships

--- practice.py
+++ practice.py
@@ -1,11 +1,30 @@
-#type casting
-num1 = 1
-num2 = 2
-word = "The sum of "
-sum = num1 + num2
+# #type casting
+# num1 = 1
+# num2 = 2
+# word = "The sum of "
+# sum = num1 + num2
 
-print(word + str(num1) + " and " +str(num2) + " is " + str(sum))
+# print(word + str(num1) + " and " +str(num2) + " is " + str(sum))
 
-#another way of calling the variables
-print(f"{word}{num1} and {num2} is {sum}") #this is the new way of calling variables
-print("%s %d and %d is %d" % (word, num1, num2, sum)) #this is the old way of calling variables
+# #another way of calling the variables
+# print(f"{word}{num1} and {num2} is {sum}") #this is the new way of calling variables
+# print("%s %d and %d is %d" % (word, num1, num2, sum)) #this is the old way of calling variables
+
+# #lists
+# list = []
+# list.append("Hello")
+# list.append("World") 
+# list.append("Goodbye")
+# print(list[0])
+# print(list[1])
+# print(list[2])
+
+# for x in list:
+#     print(x)
+
+# power relationships
+
+squared = 3**2
+cubed = 3**3
+print(squared)
+print(cubed)
